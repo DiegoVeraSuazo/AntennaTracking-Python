0ece91d60477db29b48b1e21bc8da97e9b8ba185
diego.vera <diegoverasuazo@gmail.com>
2024-07-15 15:16:59 -0400

    Añadido de logging.basicConfig

--- rot2ProgInteractor.py
+++ rot2ProgInteractor.py
@@ -24,6 +24,7 @@ class ROT2Prog:
 	"""Sends commands and receives responses from the ROT2Prog controller.
 	"""
 	_log = logging.getLogger(__name__)
+	logging.basicConfig(filename='Rot2log.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 	_ser = None
 
